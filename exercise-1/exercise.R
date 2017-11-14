b9d0306cd73af87c24ca608211e075f4dca6a363
chenduowen <chenduowen@D-69-91-176-165.dhcp4.washington.edu>
2017-11-14 13:58:28 -0800

    finish

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,9 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
-
+setwd("/Users/chenduowen/Desktop/INFO201/ch16-shiny/exercise-1")
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("scripts/BuildScatter.r")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+GetScatterMap(iris, iris$Sepal.Width, iris$Sepal.Length, "red", "Iris Data", "Sepal.Width", "Sepal.Lengh")
